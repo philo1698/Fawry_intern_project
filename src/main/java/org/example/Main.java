58e5102bfc3a459b03f7066a0750f2cc11616038
philo1698 <pilopatesameh@gmail.com>
2025-07-06 01:10:00 +0300

    create checkout function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
     }
 
     public void checkout(ArrayList<bought_product> Choosen_products, customer Customer){
